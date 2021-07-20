1fb3d6f08b3e86e386cf29022f78e21150f7ab1f
Beca Sun <65320571+becapple@users.noreply.github.com>
2021-07-20 12:37:37 +0800

    Add files via upload

---
+++ UI/js/index.js
@@ -0,0 +1,10 @@
+$(function(){
+    $("#Header").load("Header.html");
+    $("#AsideNav_A").load("../AsideNav/AsideNav_A.html");
+    $("#AsideNav_E").load("../AsideNav/AsideNav_E.html");
+    $("#main-left").load("Menu.html");
+
+
+});
+
+
